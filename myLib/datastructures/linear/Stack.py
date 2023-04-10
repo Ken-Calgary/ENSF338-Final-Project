3219b8950f19a4670a367163104331aa49eaea7c
Ken <ken.liu1@ucalgary.ca>
2023-04-10 01:03:20 -0600

    Stack is mostly implemented (not sure if insert, sortedInsert, search, etc should exist in Stack)

--- myLib/datastructures/linear/Stack.py
+++ myLib/datastructures/linear/Stack.py
@@ -1,1 +1,24 @@
 # Implemntation of Stack
+from myLib.datastructures.linear.SLL import SLL
+
+class Stack(SLL):
+    def __init__(self, head=None):
+        super().__init__(head)
+
+    def push(self, node):
+        super().insertHead(node)
+    
+    def insertTail(self, node):
+        return
+    
+    def insert(self, node, position):
+        return
+
+    def pop(self):
+        head = self.head
+        super().deleteHead()
+        return head
+    
+    def deleteTail(self):
+        return
+    
