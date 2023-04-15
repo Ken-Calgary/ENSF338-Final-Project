b2910636f45af9671c90feded54d730fee52520f
HylianMuffins <catholiczach@gmail.com>
2023-04-14 20:58:53 -0600

    added package stuff

--- build/lib/myLib/setup.py
+++ build/lib/myLib/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='trees',
+setup(name='ENSF 338 Final Project Package',
     version= '1.0',
-    description='different types of trees',
+    description='different types of datastructures',
     author='Zachariah Blair',
     author_email='zachariah.blair@gmail.com',
     packages=find_packages(),
